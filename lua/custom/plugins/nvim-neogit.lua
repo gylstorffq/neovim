2756879fe1fff3087dba973b67293d449f4d12b7
Quirin Gylstorff <quirin.gylstorff@siemens.com>
2023-03-02 21:23:20 +0100

    update plugins (remove nvim-neogit add vim-pandoc)

--- lua/custom/plugins/nvim-neogit.lua
+++
@@ -1,9 +0,0 @@
-return {
-    'TimUntersberger/neogit',
-    dependencies = {
-        'nvim-lua/plenary.nvim'
-    },
-    config = function()
-        require("neogit").setup {}
-    end
-}
---
+++ lua/custom/plugins/vim-pandoc.lua
@@ -0,0 +1,8 @@
+return {
+    'vim-pandoc/vim-pandoc',
+    dependencies = {
+        'vim-pandoc/vim-pandoc-syntax'
+	},
+    config = function()
+    end
+}
