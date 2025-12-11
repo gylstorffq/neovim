46eaef1c6ed392f112ff02e79c8c8a5889a6f828
Quirin Gylstorff <quirin.gylstorff@siemens.com>
2025-12-11 11:54:04 +0100

    Modularize init

--- lua/options.lua
+++ lua/options.lua
@@ -121,3 +121,14 @@ vim.o.sps = "file:/home/gylstorff/.config/nvim/spell/sugg,best"
 vim.cmd.append = "set spell syntax=off"
 vim.o.expandtab = true
 vim.o.tabstop = 4
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.hl.on_yank()`
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.hl.on_yank()
+	end,
+})
