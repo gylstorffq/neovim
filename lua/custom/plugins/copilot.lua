674d7c707a82ac6a6913d2975f7ce23e23e4ffc3
Quirin Gylstorff <quirin.gylstorff@siemens.com>
2025-12-11 12:37:22 +0100

    Update copilot

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,50 +1,51 @@
 return {
-	'zbirenbaum/copilot.lua',
-        build = ":Copilot auth",
-        cmd = "Copilot",
+	"zbirenbaum/copilot.lua",
+	commit = "a064570",
+	build = ":Copilot auth",
+	cmd = "Copilot",
 	config = function()
-                require("copilot").setup({
-                  panel = {
-                    enabled = true,
-                    auto_refresh = true,
-                    keymap = {
-                      jump_prev = "[[",
-                      jump_next = "]]",
-                      accept = "<CR>",
-                      refresh = "gr",
-                      open = "<M-CR>"
-                    },
-                    layout = {
-                      position = "bottom", -- | top | left | right
-                      ratio = 0.4
-                    },
-                  },
-                  suggestion = {
-                    enabled = true,
-                    auto_trigger = true,
-                    debounce = 75,
-                    keymap = {
-                      accept = "<leader>cc",
-                      accept_word = false,
-                      accept_line = false,
-                      next = "<leader>cn",
-                      prev = "<leader>cn",
-                      dismiss = "<leader>cd",
-                    },
-                  },
-                  filetypes = {
-                    yaml = false,
-                    markdown = false,
-                    help = false,
-                    gitcommit = false,
-                    gitrebase = false,
-                    hgcommit = false,
-                    svn = false,
-                    cvs = false,
-                    ["."] = false,
-                  },
-                  copilot_node_command = 'node', -- Node.js version must be > 18.x
-                  server_opts_overrides = {},
-                })
-        end
+		require("copilot").setup({
+			panel = {
+				enabled = true,
+				auto_refresh = true,
+				keymap = {
+					jump_prev = "[[",
+					jump_next = "]]",
+					accept = "<CR>",
+					refresh = "gr",
+					open = "<M-CR>",
+				},
+				layout = {
+					position = "bottom", -- | top | left | right
+					ratio = 0.4,
+				},
+			},
+			suggestion = {
+				enabled = true,
+				auto_trigger = true,
+				debounce = 75,
+				keymap = {
+					accept = "<leader>cc",
+					accept_word = false,
+					accept_line = false,
+					next = "<leader>cn",
+					prev = "<leader>cp",
+					dismiss = "<leader>cd",
+				},
+			},
+			filetypes = {
+				yaml = false,
+				markdown = false,
+				help = false,
+				gitcommit = false,
+				gitrebase = false,
+				hgcommit = false,
+				svn = false,
+				cvs = false,
+				["."] = false,
+			},
+			copilot_node_command = "node", -- Node.js version must be > 18.x
+			server_opts_overrides = {},
+		})
+	end,
 }
