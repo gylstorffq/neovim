21ca548bec5bfa5b43ed76402b4609de9b67f630
Quirin Gylstorff <quirin.gylstorff@siemens.com>
2026-01-17 20:18:01 +0100

    disable copilot on startup

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -4,48 +4,49 @@ return {
 	build = ":Copilot auth",
 	cmd = "Copilot",
 	config = function()
-		require("copilot").setup({
-			panel = {
-				enabled = true,
-				auto_refresh = true,
-				keymap = {
-					jump_prev = "[[",
-					jump_next = "]]",
-					accept = "<CR>",
-					refresh = "gr",
-					open = "<M-CR>",
-				},
-				layout = {
-					position = "bottom", -- | top | left | right
-					ratio = 0.4,
-				},
-			},
-			suggestion = {
-				enabled = true,
-				auto_trigger = true,
-				debounce = 75,
-				keymap = {
-					accept = "<leader>cc",
-					accept_word = false,
-					accept_line = false,
-					next = "<leader>cn",
-					prev = "<leader>cp",
-					dismiss = "<leader>cd",
-				},
-			},
-			filetypes = {
-				yaml = false,
-				markdown = false,
-				help = false,
-				gitcommit = false,
-				gitrebase = false,
-				hgcommit = false,
-				svn = false,
-				cvs = false,
-				["."] = false,
-			},
-			copilot_node_command = "node", -- Node.js version must be > 18.x
-			server_opts_overrides = {},
-		})
+		vim.cmd("Copilot disable")
 	end,
+	opts = {
+		panel = {
+			enabled = true,
+			auto_refresh = true,
+			keymap = {
+				jump_prev = "[[",
+				jump_next = "]]",
+				accept = "<CR>",
+				refresh = "gr",
+				open = "<M-CR>",
+			},
+			layout = {
+				position = "bottom", -- | top | left | right
+				ratio = 0.4,
+			},
+		},
+		suggestion = {
+			enabled = true,
+			auto_trigger = true,
+			debounce = 75,
+			keymap = {
+				accept = "<M-c>",
+				accept_word = false,
+				accept_line = false,
+				next = "<M-n>",
+				prev = "<M-p>",
+				dismiss = "<M-d>",
+			},
+		},
+		filetypes = {
+			yaml = false,
+			markdown = false,
+			help = false,
+			gitcommit = false,
+			gitrebase = false,
+			hgcommit = false,
+			svn = false,
+			cvs = false,
+			["."] = false,
+		},
+		copilot_node_command = "node", -- Node.js version must be > 18.x
+		server_opts_overrides = {},
+	},
 }
